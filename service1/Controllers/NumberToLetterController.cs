053a8381c3a40613209af4141e86f2da7320d1ce
Miguel Teheran <miguelteheran@gmail.com>
2021-11-16 16:32:19 -0500

    adding base project and first microservice

---
+++ service1/Controllers/NumberToLetterController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Humanizer;
+namespace service1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NumberToLetterController : ControllerBase
+{
+
+        [HttpGet("{number}")]
+        public async Task<ActionResult<string>> ConvertToLetter(int number)
+        {
+            string result = number.ToWords(false, new System.Globalization.CultureInfo("ES-es"));
+            return Ok(result);
+        }
+   
+}
